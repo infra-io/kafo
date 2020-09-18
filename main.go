49626ffc81b90188f73a01953f91bedd26e20acc
FishGoddess <1149062639@qq.com>
2020-09-18 23:18:39 +0800

    加入 GC 机制，随机淘汰过期数据

--- main.go
+++ main.go
@@ -19,11 +19,16 @@ func main() {
 
 	// Parse all flags
 	address := flag.String("address", ":5837", "The address used to listen, such as 127.0.0.1:5837.")
-	maxEntrySize := flag.Int64("maxEntrySize", caches.DefaultMaxEntrySize, "The max memory size that entries can use.")
+
+	options := caches.DefaultOptions()
+	flag.Int64Var(&options.MaxEntrySize, "maxEntrySize", options.MaxEntrySize, "The max memory size that entries can use. The unit is GB.")
+	flag.IntVar(&options.MaxGcCount, "maxGcCount", options.MaxGcCount, "The max count of entries that gc will clean.")
+	flag.Int64Var(&options.GcDuration, "gcDuration", options.GcDuration, "The duration between two gc tasks. The unit is Minute.")
 	flag.Parse()
 
 	// Initialize
-	cache := caches.NewCacheWithMaxEntrySize(*maxEntrySize)
+	cache := caches.NewCacheWith(options)
+	cache.AutoGc()
 	err := servers.NewHTTPServer(cache).Run(*address)
 	if err != nil {
 		panic(err)
